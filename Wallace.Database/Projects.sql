d9faf853b40089601953ffef127478e24820cbb4
Dawson Gilmore <d.gilmore@socyinc.com>
2018-11-21 18:28:28 -0500

    First 4 database tables added

---
+++ Wallace.Database/Projects.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [dbo].[Projects]
+(
+	[pId] INT NOT NULL PRIMARY KEY,
+	[pName] varchar(50) NOT NULL,
+	[pBudget] INT NOT NULL,
+	[pDesc] varchar(200) NOT NULL
+)
---
+++ Wallace.Database/Relations/VersionSpecs.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [dbo].[VersionSpecs]
+(
+	[vId] INT NOT NULL FOREIGN KEY REFERENCES Versions(vId),
+	[sId] INT NOT NULL FOREIGN KEY REFERENCES Specifications(sId),
+	[Done] bit NOT NULL,
+	PRIMARY KEY (vId, sId)
+)
