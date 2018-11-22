e97f8cb3e4cd0f11b6ff7afeee13f86fdde45bf8
Dawson Gilmore <d.gilmore@socyinc.com>
2018-11-22 08:30:23 -0500

    Somehow Wallace.Common can be referenced now

--- Wallace.UI/Controllers/HomeController.cs
+++ Wallace.UI/Controllers/HomeController.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Wallace.UI.Models;
+
 namespace Wallace.UI.Controllers
 {
     public class HomeController : Controller
