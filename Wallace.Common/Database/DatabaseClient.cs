447492ba543c5cb639790f3b8ca0984b8e9a7b08
Dawson Gilmore <d.gilmore@socyinc.com>
2018-11-22 19:29:01 -0500

    Version table in Database Client

--- Wallace.Common/Database/DatabaseClient.cs
+++ Wallace.Common/Database/DatabaseClient.cs
@@ -3,28 +3,33 @@ using System.Collections.Generic;
 using System.Text;
 using System.Data.SqlClient;
 using Wallace.Common.Models;
-using Wallace.Common.Database.DBModels;
 namespace Wallace.Common.Database
 {
     
     public class DatabaseClient
     {
-        private string query;
+        private const string getProjectsQuery = @"  SeLeCt
+                                                        *
+                                                    FrOm
+                                                        Projects";
+        private const string getVersionsString = @" SeLeCt
+                                                        *
+                                                    FrOm
+                                                        Versions
+                                                    WhErE
+                                                        pid = @pid";
+        
         private SqlCommand cmd;
         private SqlConnection conn;
+
         public DatabaseClient()
         {
             conn = new SqlConnection("Data Source=.;Initial Catalog=Wallace;Integrated Security=True");
         }
-        //"Data Source=SEAN-LAPTOP;Initial Catalog=Wallace;Integrated Security=True"
-        //"Data Source=DESKTOP-IO1PN0S;Initial Catalog=Wallace;Integrated Security=True"
+
         public List<DBProject> getProjects(){
             List<DBProject> projects = new List<DBProject>();
-            query = @"  SELECT
-                            *
-                        FROM
-                            Projects";
-            cmd = new SqlCommand(query, conn);
+            cmd = new SqlCommand(getProjectsQuery, conn);
             try
             {
                 conn.Open();
@@ -32,7 +37,7 @@ namespace Wallace.Common.Database
                 while (reader.Read())
                 {
                     DBProject currProject = new DBProject();
-                    currProject.pid = !reader.IsDBNull(0) ? reader.GetInt32(0) : -1;
+                    currProject.id = !reader.IsDBNull(0) ? reader.GetInt32(0) : -1;
                     currProject.name = !reader.IsDBNull(1) ? reader.GetString(1) : "";
                     currProject.budget = !reader.IsDBNull(2) ? reader.GetInt32(2) : 0;
                     currProject.desc = !reader.IsDBNull(3) ? reader.GetString(3) : "";
@@ -47,6 +52,34 @@ namespace Wallace.Common.Database
                 conn.Close();
             }
             return projects;
+        }
+
+        public List<DBVersion> getVersions(DBProject p)
+        {
+            int id = p.id;
+            List<DBVersion> versions = new List<DBVersion>();
+            cmd = new SqlCommand(getVersionsString, conn);
+            try
+            {
+                conn.Open();
+                cmd.Parameters.AddWithValue("@pid", id);
+                SqlDataReader reader = cmd.ExecuteReader();
+                while (reader.Read())
+                {
+                    DBVersion currVersion = new DBVersion();
+                    currVersion.id = !reader.IsDBNull(0) ? reader.GetInt32(0) : -1;
+                    currVersion.vnum = !reader.IsDBNull(2) ? reader.GetInt32(2) : -1;
+                    currVersion.release = !reader.IsDBNull(3) ? reader.GetDateTime(3) : DateTime.UtcNow;
+
+                    versions.Add(currVersion);
+                }
+            }
+            finally
+            {
+                conn.Close();
+            }
+
+            return versions;
         }
     }
 }
